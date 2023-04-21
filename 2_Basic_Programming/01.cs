70c9c6b89df77230233f86e9a4e4a37faedb2708
Tulakorn Sawangmuang <sunnytulakorn@gmail.com>
2023-04-21 15:47:33 +0700

    complete basic programming code HW

--- 2_Basic_Programming/01.cs
+++ 2_Basic_Programming/01.cs
@@ -1,33 +1,41 @@
 internal class NumberGuessingGame
 {
-    public void Start()
+    /// <summary>
+    /// Input 4 numbers to guess the random numbers.
+    /// </summary>
+    static void Start()
     {
-        char[] answerSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char[] answerSets = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
+        int indexStart = 0;
         var rnd = new Random();
-        for (var i = 0; i < answerSet.Length; i++)
+
+        //loop for Swap member values in the answerSets array.
+        for (var index = 0; index < answerSets.Length; index++)
         {
-            var x = rnd.Next(0, answerSet.Length);
-            var y = rnd.Next(0, answerSet.Length);
-            var z = answerSet[x];
-            answerSet[x] = answerSet[y];
-            answerSet[y] = z;
+            var rnd1 = rnd.Next(indexStart, answerSets.Length);
+            var rnd2 = rnd.Next(indexStart, answerSets.Length);
+            var changePosition = answerSets[rnd1];
+            answerSets[rnd1] = answerSets[rnd2];
+            answerSets[rnd2] = changePosition;
         }
 
+        //random asnwer number
         char[] quiz = new char[4];
-        for (var i = 0; i < 4; i++)
+        for (var index = 0; index < 4; index++)
         {
-            quiz[i] = answerSet[i];
+            quiz[index] = answerSets[index];
         }
 
         Console.WriteLine("Welcome to guessing number game. We have unique number in 4 digits.");
-        int k = 1;
-        while (k <= 3)
+        // number of round you can guess the number
+        int round = 1;
+        while (round <= 3)
         {
             Console.Write("You guess: ");
             var inp = Console.ReadLine();
 
-            if (string.IsNullOrEmpty(inp)) 
+            if (string.IsNullOrEmpty(inp))
             {
                 Console.WriteLine("Invalid number");
             }
@@ -35,26 +43,27 @@ internal class NumberGuessingGame
             {
                 if (inp.Length != 4)
                 {
-                    Console.WriteLine("Must be 4 digits");
+                    Check4Digits();
                 }
                 else
                 {
                     if (inp.Any(c => !char.IsDigit(c)))
                     {
-                        Console.WriteLine("Must be 4 digits");
+                        Check4Digits();
                     }
                     else
                     {
                         var correct_count = 0;
                         var wrong_pos_count = 0;
-                        for (var i = 0; i < 4; i++)
+                        // loop for check position between quiz and your guess
+                        for (var index = 0; index < 4; index++)
                         {
-                            if (inp[i] == quiz[i])
+                            if (inp[index] == quiz[index])
                                 correct_count++;
-                            else if (quiz.Contains(inp[i]))
+                            else if (quiz.Contains(inp[index]))
                                 wrong_pos_count++;
                         }
-
+                        //when 4 number of your guessing is correct
                         if (correct_count == 4)
                         {
                             Console.WriteLine("You WIN!!");
@@ -62,7 +71,7 @@ internal class NumberGuessingGame
                         }
 
                         Console.WriteLine($"Wrong! (Correct = {correct_count}, contain but wrong position = {wrong_pos_count})");
-                        k++;
+                        round++;
                     }
                 }
             }
@@ -70,10 +79,23 @@ internal class NumberGuessingGame
 
         Console.WriteLine("You LOSE +_+");
         Console.Write("Answer: ");
-        for (var i = 0; i < 4; i++)
+        //loop for show ture answer of 4 digits
+        for (var index = 0; index < 4; index++)
         {
-            Console.Write(quiz[i]);
+            Console.Write(quiz[index]);
         }
         Console.Write(Environment.NewLine);
+    }
+
+    /// <summary>
+    /// display "Must be 4 digits" 
+    /// </summary>
+    public static void Check4Digits()
+    {
+        Console.WriteLine("Must be 4 digits");
+    }
+    static void Main(string[] args)
+    {
+        Start();
     }
 }
--- 2_Basic_Programming/02.cs
+++ 2_Basic_Programming/02.cs
@@ -9,144 +9,157 @@ class Equipment
 
 class RefineCalculator
 {
+    /// <summary>
+    /// Get equipment enhancement results
+    /// </summary>
+    /// <param name="equipments"></param>
+    /// <param name="isVIP"></param>
     public void RefineEquipments(List<Equipment> equipments, bool isVIP)
     {
+        var r = new Random();
+        var x = r.Next(0, 100);
         for (var i = 0; i < equipments.Count(); i++)
         {
             var e = equipments[i];
             if (e.ItemLevel < 10)
             {
-                if (e.Type == "Weapon")
+                RefineEquipment(e, isVIP, x);
+            }
+        }
+    }
+    //Equipment enhancement classification function
+    public void RefineEquipment(Equipment e, bool isVIP, int x)
+    {
+        if (e.Type == "Weapon")
+        {
+            RefineWeapond(e, isVIP, x);
+        }
+        else if (e.Type == "Armor")
+        {
+            RefineArmor(e, isVIP, x);
+        }
+    }
+    //Calculate weapon enhancement
+    public void RefineWeapond(Equipment e, bool isVIP, int x)
+    {
+        if (e.ItemLevel == 1)
+        {
+            if (e.RefineLevel < 7)
+            {
+                e.RefineLevel = RefineSucceed(e);
+            }
+            else if (e.RefineLevel < 9)
+            {
+                if (x < 60)
+                    e.RefineLevel = RefineSucceed(e);
+                else
                 {
-                    if (e.ItemLevel == 1)
-                    {
-                        if (e.RefineLevel < 7)
-                        {
-                            e.RefineLevel = e.RefineLevel + 1;
-                        }
-                        else if (e.RefineLevel < 9)
-                        {
-                            var r = new Random();
-                            var x = r.Next(0, 100);
-                            if (x < 60)
-                                e.RefineLevel = e.RefineLevel + 1;
-                            else
-                            {
-                                if (isVIP)
-                                    e.RefineLevel = e.RefineLevel - 1;
-                                else
-                                    e.RefineLevel = 0;
-                            }
-
-                        }
-                        else
-                        {
-                            var r = new Random();
-                            var x = r.Next(0, 100);
-                            if (x < 20)
-                                e.RefineLevel = e.RefineLevel + 1;
-                            else
-                            {
-                                if (isVIP)
-                                    e.RefineLevel = e.RefineLevel - 1;
-                                else
-                                    e.RefineLevel = 0;
-                            }
-                        }
-                    }
-                    else if (e.ItemLevel == 2)
-                    {
-                        if (e.RefineLevel < 7)
-                        {
-                            e.RefineLevel = e.RefineLevel + 1;
-                        }
-                        else if (e.RefineLevel < 9)
-                        {
-                            var r = new Random();
-                            var x = r.Next(0, 100);
-                            if (x < 30)
-                                e.RefineLevel = e.RefineLevel + 1;
-                            else
-                            {
-                                if (isVIP)
-                                    e.RefineLevel = e.RefineLevel - 1;
-                                else
-                                    e.RefineLevel = 0;
-                            }
-                        }
-                        else
-                        {
-                            var r = new Random();
-                            var x = r.Next(0, 100);
-                            if (x < 15)
-                                e.RefineLevel = e.RefineLevel + 1;
-                            else
-                            {
-                                if (isVIP)
-                                    e.RefineLevel = e.RefineLevel - 1;
-                                else
-                                    e.RefineLevel = 0;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (e.RefineLevel < 5)
-                        {
-                            e.RefineLevel = e.RefineLevel + 1;
-                        }
-                        else if (e.RefineLevel < 7)
-                        {
-                            var r = new Random();
-                            var x = r.Next(0, 100);
-                            if (x < 40)
-                                e.RefineLevel = e.RefineLevel + 1;
-                            else
-                            {
-                                if (isVIP)
-                                    e.RefineLevel = e.RefineLevel - 1;
-                                else
-                                    e.RefineLevel = 0;
-                            }
-                        }
-                        else
-                        {
-                            var r = new Random();
-                            var x = r.Next(0, 100);
-                            if (x < 10)
-                                e.RefineLevel = e.RefineLevel + 1;
-                            else
-                            {
-                                if (isVIP)
-                                    e.RefineLevel = e.RefineLevel - 1;
-                                else
-                                    e.RefineLevel = 0;
-                            }
-                        }
-                    }
+                    CheckVip(e, isVIP);
                 }
-                else if (e.Type == "Armor")
+
+            }
+            else
+            {
+                if (x < 20)
+                    e.RefineLevel = RefineSucceed(e);
+                else
                 {
-                    if (e.RefineLevel < 5)
-                    {
-                        e.RefineLevel = e.RefineLevel + 1;
-                    }
-                    else
-                    {
-                        var r = new Random();
-                        var x = r.Next(0, 100);
-                        if (x < (10 - e.RefineLevel) * 10)
-                            e.RefineLevel = e.RefineLevel + 1;
-                        else
-                        {
-                            if (isVIP)
-                                e.RefineLevel = e.RefineLevel - 1;
-                            else
-                                e.RefineLevel = 0;
-                        }
-                    }
+                    CheckVip(e, isVIP);
                 }
             }
         }
+        else if (e.ItemLevel == 2)
+        {
+            if (e.RefineLevel < 7)
+            {
+                e.RefineLevel = RefineSucceed(e);
+            }
+            else if (e.RefineLevel < 9)
+            {
+                if (x < 30)
+                    e.RefineLevel = RefineSucceed(e);
+                else
+                {
+                    CheckVip(e, isVIP);
+                }
+            }
+            else
+            {
+                if (x < 15)
+                    e.RefineLevel = RefineSucceed(e);
+                else
+                {
+                    CheckVip(e, isVIP);
+                }
+            }
+        }
+        else
+        {
+            if (e.RefineLevel < 5)
+            {
+                e.RefineLevel = RefineSucceed(e);
+            }
+            else if (e.RefineLevel < 7)
+            {
+                if (x < 40)
+                    e.RefineLevel = RefineSucceed(e);
+                else
+                {
+                    CheckVip(e, isVIP);
+                }
+            }
+            else
+            {
+                if (x < 10)
+                    e.RefineLevel = RefineSucceed(e);
+                else
+                {
+                    CheckVip(e, isVIP);
+                }
+            }
+        }
+    }
+    //Calculate armor enhancement
+    public void RefineArmor(Equipment e, bool isVIP, int x)
+    {
+        if (e.RefineLevel < 5)
+        {
+            e.RefineLevel = RefineSucceed(e);
+        }
+        else
+        {
+            if (x < (10 - e.RefineLevel) * 10)
+                e.RefineLevel = RefineSucceed(e);
+            else
+            {
+                CheckVip(e, isVIP);
+            }
+        }
+    }
+    //Check VIP player
+    public void CheckVip(Equipment e, bool isVIP)
+    {
+        if (isVIP)
+            e.RefineLevel = RefineFailed(e);
+        else
+            e.RefineLevel = RefineRecovry(e);
+    }
+    //Calculate the enhancement value when the enhancement is successful.
+    public int RefineSucceed(Equipment e)
+    {
+        var equipmentLevel = e.RefineLevel + 1;
+        return equipmentLevel;
+    }
+    //Calculate the enhancement value when the enhancement is unsuccessful and is VIP player.
+    public int RefineFailed(Equipment e)
+    {
+        var equipmentLevel = e.RefineLevel - 1;
+        return equipmentLevel;
+    }
+    //Calculate the enhancement value when the enhancement is unsuccessful and is not VIP player.
+    public int RefineRecovry(Equipment e)
+    {
+        var equipmentLevel = 0;
+        return equipmentLevel;
     }
 }
